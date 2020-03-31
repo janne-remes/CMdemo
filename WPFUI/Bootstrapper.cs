caf0282950a9f9bac94f77fed4ca0e11bcc819f3
janne-remes <janneremes@outlook.com>
2020-03-31 21:38:59 +0300

    Add project files.

---
+++ WPFUI/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
